d2aaaa4ea4c9e4cc288c703eac2747cb7e17a186
Kleber Germano <kleber_lsgermano@hotmail.com>
2019-12-27 03:49:10 -0300

    Update configNodemailer_example.js

--- server/nodemailer/configNodemailer_example.js
+++ server/nodemailer/configNodemailer_example.js
@@ -4,13 +4,13 @@ const configNodemailer = {
     pass: "your_email_password"
   },
   smtp: {
-    host: "smtp-mail.outlook.com", //smtp host of your email service// as exaple was used the Microsoft Outlook smtp service
+    host: "smtp-mail.outlook.com", //smtp host of your email service// as example was used the Microsoft Outlook smtp service
     port: 587 //port of smtp service/ 587 is the most common
   },
 
   info: {
-    from: '"Website form contact <>"', // sender address
-    to: " <you_email_user01@email.com>, <you_email_user02@email.com>", // list of receivers // you can config to send to more than one
+    from: '"Website form contact <your_email_user@example.com>"', // sender address
+    to: " <you_email_user01@email.com>, <you_email_user02@email.com>", // list of receivers // you can config more than one to receive it
     subject: "New contact from the website"
   }
 };
